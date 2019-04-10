7dd1b1c91c6d5822590c086e93fc0c85b42a6871
Jesus Marinez Ruano <jesus.martinez.ruano@gmail.com>
2019-04-10 21:39:58 +0200

    genPDF done

--- react-redux-client/src/actions/genPDF.js
+++ react-redux-client/src/actions/genPDF.js
@@ -1,3 +1,6 @@
+import jsPDF from 'jspdf';
+
+
 export function genPDF(proyectorId) {
     console.log(proyectorId);
     fetch(document.location.origin+'/api/'+proyectorId)
@@ -9,7 +12,19 @@ export function genPDF(proyectorId) {
     })   
  }
  function printPDF(proData){
-    console.log("PrintPDF : ",proData);
-    
+    console.log("PrintPDF : ",proData.ubiAlias);
+        var doc = new jsPDF({
+        orientation: "l",
+        unit: "mm"
+        });
+
+        doc.text(proData.ubiAlias, 130, 20);
+        doc.text(proData.ubiInvent, 130, 40);
+
+        let dataSrc = doc.output('datauristring');
+        let w= window.open("", "MsgWindow");
+        w.document.write("<html><head><title>jsPDF</title></head><body><embed src=" + 
+        dataSrc + " width='100%' height='100%'></embed></body></html>");
+
  }
  
